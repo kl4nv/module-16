f07b1b93039ef64ff4da1b828183606543c8f036
kl4nv <kupull94@bk.ru>
2024-12-05 09:23:17 +1000

    m_16_2.py

--- m_16_2.py
+++ m_16_2.py
@@ -12,16 +12,21 @@ async def root() -> str:
 async def admin_root() -> str:
     return 'Вы вошли как администратор'
 
-
 @app.get('/user/{user_id}')
-async def user_root(user_id: int = Path(ge=1, le=100, description='Enter User ID', example='5')) -> str:
+async def user_root(user_id: Annotated[int, Path(...,
+                                        ge=1,
+                                        le=100,
+                                        description='Enter User ID',
+                                        example='10')]):
     return f'Вы вошли как пользователь {user_id}'
 
-
 @app.get('/user/{username}/{age}')
-async def user_info(username: Annotated[str, Path(min_length=5,
-                                         max_length=20,
-                                         description='Enter username',
-                                         example='UrbanUser')],
-                    age: int = Path(ge=18, le=120, description='Enter age', example='24')) -> str:
+async def user_info(username: Annotated[str, Path(..., min_length=5,
+                                                  max_length=20,
+                                                  description='Enter username',
+                                                  example='UrbanUser')],
+                    age: Annotated[int, Path(..., ge=18,
+                                             le=120,
+                                             description='Enter age',
+                                             example='24')]):
     return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
